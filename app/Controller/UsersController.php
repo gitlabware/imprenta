9341b5858257e33eb8e76dd8fd9b3351cbd8f16f
eynar <eynar@labware.com.bo>
2015-01-20 16:12:01 -0400

    Correccion de un back del logeo

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -9,7 +9,7 @@ class UsersController extends AppController {
 
     public function beforeFilter(){
         parent::beforeFilter();
-        $this->Auth->allow();
+        $this->Auth->allow('login2');
     }
 
     public function index() {
