757c2dcf08aa8be728d10adda748808633739982
Labware <mercedes@labware.com.bo>
2015-01-23 18:00:06 -0400

    Altas, Bajas y modificaciones de costos y adicion al menu

--- app/Controller/CostosController.php
+++ app/Controller/CostosController.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppController', 'Controller');
+
 /**
  * Costos Controller
  *
@@ -8,101 +10,74 @@ App::uses('AppController', 'Controller');
  */
 class CostosController extends AppController {
 
-/**
- * Components
- *
- * @var array
- */
-	public $components = array('Paginator');
-	public $layout = 'imprenta';
-	public $uses = array('Costo');
+    /**
+     * Components
+     *
+     * @var array
+     */
+    public $components = array('Paginator');
+    public $layout = 'imprenta';
+    public $uses = array('Costo');
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		/*$this->Costo->recursive = 0;
-		$this->set('costos', $this->Paginator->paginate());*/
-		$costos = $this->Costo->find('all');
-		$this->set(compact('costos'));
-	}
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index() {
+        /* $this->Costo->recursive = 0;
+          $this->set('costos', $this->Paginator->paginate()); */
+        $costos = $this->Costo->find('all');
+        $this->set(compact('costos'));
+    }
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->Costo->exists($id)) {
-			throw new NotFoundException(__('Invalid costo'));
-		}
-		$options = array('conditions' => array('Costo.' . $this->Costo->primaryKey => $id));
-		$this->set('costo', $this->Costo->find('first', $options));
-	}
 
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Costo->create();
-			if ($this->Costo->save($this->request->data)) {
-				$this->Session->setFlash(__('The costo has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The costo could not be saved. Please, try again.'));
-			}
-		}
-	}
+    public function add() {
+        
+        $this->layout='ajax';
+        if ($this->request->is('post')) {
+            //debug($this->request->data); exit;
+            $this->Costo->create();
+            if ($this->Costo->save($this->request->data)) {
+                $this->Session->setFlash ('El costo se registro correctamente','msgbueno');
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash('Error al registrar', 'msgerror');
+            }
+        }
+    }
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		if (!$this->Costo->exists($id)) {
-			throw new NotFoundException(__('Invalid costo'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->Costo->save($this->request->data)) {
-				$this->Session->setFlash(__('The costo has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The costo could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('Costo.' . $this->Costo->primaryKey => $id));
-			$this->request->data = $this->Costo->find('first', $options);
-		}
-	}
+    public function edit($id = null) {
+        $this->layout='ajax';
+        if (!$this->Costo->exists($id)) {
+            throw new NotFoundException('Costo invalido', 'msgerror');
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->Costo->save($this->request->data)) {
+                $this->Session->setFlash('Modificacion correcta', 'msgbueno');
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash('No se pudo guardar', 'msgerror');
+            }
+        } else {
+            $options = array('conditions' => array('Costo.' . $this->Costo->primaryKey => $id));
+            $this->request->data = $this->Costo->find('first', $options);
+        }
+    }
 
-/**
- * delete method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		$this->Costo->id = $id;
-		if (!$this->Costo->exists()) {
-			throw new NotFoundException(__('Invalid costo'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->Costo->delete()) {
-			$this->Session->setFlash(__('The costo has been deleted.'));
-		} else {
-			$this->Session->setFlash(__('The costo could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
+    public function delete($id = null) {
+        $this->Costo->id = $id;
+        if (!$this->Costo->exists()) {
+            //throw new NotFoundException('Costo Invalido');
+            $this->Session->setFlash('Costo Invalido');
+        }
+        //$this->request->allowMethod('post', 'delete');
+        if ($this->Costo->delete()) {
+            $this->Session->setFlash('Se elimino correctamente', 'msgbueno');
+        } else {
+            $this->Session->setFlash('no se pudo eliminar','msgerror');
+        }
+        return $this->redirect(array('action' => 'index'));
+    }
+
 }
--- app/View/Costos/index.ctp
+++ app/View/Costos/index.ctp
@@ -1,14 +1,14 @@
 <div class="row">
   <div class="col-md-12">
-    <h2>Listado de Usuarios</h2>
+    <h2>Listado de Costos</h2>
     <br>
     <table class="table table-bordered datatable" id="tablainsumos">
       <thead>
       <tr>
         <th>Id</th>
         <th>Nombre</th>
-        <th>Username</th>
-        <th>Tipo</th>
+        <th>Precio de Compra</th>
+        <th>Rendimiento</th>
         <th>Acciones</th>
       </tr>
       </thead>
@@ -20,9 +20,9 @@
           <td><?php echo $c['Costo']['preciocompra']; ?></td>
           <td><?php echo $c['Costo']['rendimiento']; ?></td>
           <td><a href="javascript:"
-                 onclick="cargarmodal('<?php echo $this->Html->url(array('action' => 'usuario', $c['Costo']['id'])) ?>');"
+                 onclick="cargarmodal('<?php echo $this->Html->url(array('action' => 'edit', $c['Costo']['id'])) ?>');"
                  class="btn btn-default btn-sm btn-icon icon-left"><i class="entypo-pencil"></i>Editar</a>
-            <?php echo $this->Html->link('<i class="entypo-cancel"></i>Eliminar', array('action' => 'delete', $c['Costo']['id']), array('class' => 'btn btn-danger btn-sm btn-icon icon-left', 'escape' => false, 'confirm' => 'Esta seguro de eliminar al usuario ' . $c['Costo']['id'])); ?>
+            <?php echo $this->Html->link('<i class="entypo-cancel"></i>Eliminar', array('action' => 'delete', $c['Costo']['id']), array('class' => 'btn btn-danger btn-sm btn-icon icon-left', 'escape' => false, 'confirm' => 'Esta seguro de eliminar el costo ' . $c['Costo']['id'])); ?>
           </td>
         </tr>
       <?php endforeach; ?>
